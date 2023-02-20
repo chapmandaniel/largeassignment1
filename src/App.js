62525e56554d93ab25f558617938338efb08473d
Daniel Chapman <83991470+chapmandaniel@users.noreply.github.com>
2023-02-20 00:12:48 -0330

    Large Assignment 1 Initial Push

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import Navigation from "./components/Navigation";
 import Movie from "./components/Movie";
 
-let movieTitle = "Pee Wee Herman's Big Adventure";
+let testTitle = "Pee Wee Herman's Big Adventure";
 
 function App() {
   return (
     <div className="App">
         <Navigation />
-        <Movie movieTitle={movieTitle}/>
+        <Movie movieTitle={testTitle}/>
     </div>
   );
 }
