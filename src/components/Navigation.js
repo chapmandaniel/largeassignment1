8878cef9bc5a1a9c25e390e7762ea3fa4fbfe1b8
Daniel Chapman <83991470+chapmandaniel@users.noreply.github.com>
2023-02-20 00:11:35 -0330

    Large Assignment 1 Initial Push

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+
+function Navigation() {
+  return (
+    <nav className="pageNavigation">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
